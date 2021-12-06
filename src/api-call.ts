3e44270dbdaddcdad3211fb9db2f909ad2f79dec
nmerenbloom <nmerenbloom@costar.com>
2021-12-06 11:21:46 -0500

    cleanup; edited workflow.yaml

--- src/api-call.ts
+++ src/api-call.ts
@@ -15,15 +15,10 @@ export const callGetAllCountries = async (sort: SortDetails) => {
 export const callCountryDetails = async (country: { name: string }) => {
     try {
         const response = await axios.post("/api/details", { ...country });
-        console.log(response);
-        // if (response.statusText === "GOOD_DATA") {
         if (response.data.type === 'primary') {
             return response.data.dataObject;
-            // } else if (response.statusText === "ALTERNATE_DATA") {
         } else if (response.data.type === 'alternate') {
             return AlternateDataMapper(response.data.dataObject);
-        } else {
-            console.log('response statusText had no match');
         }
     } catch (error) {
         console.log(error);
